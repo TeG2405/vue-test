cdf3a6244164668a60abfc286d4f5f64fa9e2f7a
ZvezdilinAK <artem@intervolga.ru>
2020-10-20 02:20:18 +0400

    TextWithImage

---
+++ src/views/TextWithImage/TextWithImage.vue
@@ -0,0 +1,35 @@
+<template>
+<div :class="$style.root">
+  <img :class="$style.img" :src="src" :alt="alt">
+  {{text}}
+</div>
+</template>
+
+<script>
+export default {
+  name: 'TextWithImage',
+  props: {
+    alt: {type: String, defaultValue: 'Картинка'},
+    src: {type: String, defaultValue: 'http://placehold.it/100x100'},
+    text: {type: String, defaultValue: 'Текст к картинке'}
+  },
+}
+</script>
+
+<style module lang="scss">
+.root{
+  position: relative;
+  &:after{
+    content: '';
+    display: table;
+    clear: both;
+  }
+  .img{
+    display: block;
+    max-width: 50%;
+    margin-right: 10px;
+    margin-bottom: 10px;
+    float: left;
+  }
+}
+</style>
--- src/views/index.js
+++ src/views/index.js
@@ -3,11 +3,13 @@ import {get, keys, mapValues} from 'lodash';
 import Paragraph from '@/views/Paragraph/Paragraph';
 import Slider from '@/views/Slider/Slider';
 import Banner from '@/views/Banner/Banner';
+import TextWithImage from '@/views/TextWithImage/TextWithImage';
 
 const Components = {
   Paragraph,
   Slider,
   Banner,
+  TextWithImage,
 };
 
 export const getComponentByName = (name) => get(Components, [name], null);
